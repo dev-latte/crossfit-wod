ea120f0351646541c5d6eea1b6ede775061237e4
dev-latte <dev.vlatte@gmail.com>
2022-06-06 00:22:56 +0900

    add > create WOD data for database (-ing)

--- src/apis/IsValidation.js
+++ src/apis/IsValidation.js
@@ -1,9 +1,15 @@
 export const isValidateCountNumber = (number) => {
     let result = number;
-
+    
+    console.log(number);
     // 후에 input type="number"값에 e, E가 삽입되는 현상 수정할 것
+    if(result === "0") result = "";                     // 초기값 세팅
     if(result < 0) result = Math.abs(result);           // 음수 체크
     if(result % 1 !== 0) result = Math.floor(result);   // 소수점 체크
 
     return result;
+}
+
+export const isNull = (object) => {
+    return typeof object === "undefined" || object === undefined || object === null; 
 }
--- src/components/Template/WodCard.js
+++ src/components/Template/WodCard.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styledComponents from "styled-components";
 import Button from "../UI/Button";
 
+import { HiUsers, HiUserGroup } from "react-icons/hi"
+import { isNull, isValidateCountNumber } from "../../apis/IsValidation";
+
 const StyledWodCard = styledComponents.div`
     width: 350px;
-    height: 600px;
+    height: auto;
     border: 1px solid black;
     border-radius: 16px;
-
+    padding: 10px;
 `;
 
 const StyledMovementBox = styledComponents.div`
@@ -18,14 +21,67 @@ const StyledMovementBox = styledComponents.div`
 
 const WodCard = ({ data }) => {
     const {type, isTeam, movements} = data;
-    const [teamOf, setTeamOf] = useState(2);
-    const [typeCount, setTypeCount] = useState();
+    const [teamOf, setTeamOf] = useState();
+    const [typeCount, setTypeCount] = useState(1);
     const [level, setLevel] = useState("rxd");
-    const [recordData, setRecordData] = useState([]);   // wod 기록 
-    
+
+    const movementDataDefault = {};
+    movements.forEach((value, key) => movementDataDefault[key] = {
+        goal: 1,
+        "goal-unit": value.unit[0],
+        weight: 1,
+        "weight-unit": "lb"
+    });
+
+    const [movementData, setMovementData] = useState(movementDataDefault);
+    const [recordData, setRecordData] = useState({});   // wod 기록
+
     const test = () => {
+
+
+        if(isTeam && isNull(teamOf)) {
+            alert("인원을 선택해주세요");
+            return;
+        }
+
+        if(isNull(typeCount)) {
+            alert("와드 정보를 입력해주세요.");
+        }
+
+
         // 내일은 와드 기록 insert부터 작업하기
-        console.log(type, teamOf, typeCount, level);
+        console.log(type, teamOf, typeCount, level, teamOf);
+    }
+
+    const onChangeMovementData = (e) => {
+        //원하는 데이터 map / key = movement id / value = { goal, goal-unit, weight, weight-unit }
+        const key = e.target.name;
+        const target = e.target.id;
+        console.log(e.target.name);
+        console.log(movementData);
+
+        let inputKey;
+        
+        if(target.includes("goal-unit")){
+            inputKey = "goal-unit";
+        } else if(target.includes("goal")){
+            inputKey = "goal";
+        } else if(target.includes("weight-unit")) {
+            inputKey = "weight-unit";
+        } 
+        else if(target.includes("weight")) {
+            inputKey = "weight";
+        }
+
+        let result = isNaN(e.target.value) ? e.target.value : isValidateCountNumber(e.target.value);
+
+        const data = {...movementData, [key]: {...movementData[key], [inputKey]: result}};
+        setMovementData(data);
+    }
+
+    const createMovementData = (e) => {
+        console.log(type, isTeam, typeCount, level, movementData);
+
     }
 
     // Crossfit Total 같은 경우는 다른 방식으로 카드를 보여줄 예정, 지금은 신경쓰지 않기
@@ -34,19 +90,23 @@ const WodCard = ({ data }) => {
             <p>Workout of the Day!</p>
             <div>
                 <label htmlFor="type-count">{type}</label>
-                <input type="number" name="type-count" id="type-count" onChange={e => setTypeCount(e.target.value)} value={typeCount}/>
+                <input 
+                    type="number" 
+                    name="type-count" 
+                    id="type-count" 
+                    onChange={e => setTypeCount(isValidateCountNumber(e.target.value))}
+                    value={typeCount}/>
                 {type === "For Time of" ? "Round" : "Minute"}
             </div>
             {
                 isTeam && 
                         <div>
-                            {/* 나중에는 입력이 아니라 아이콘으로 선택하게(2명 혹은 3명 수준으로) */}
                             <label htmlFor="team-of">Team of </label>
-                            <input type="number" name="team-of" id="team-of" onChange={e => setTeamOf(e.target.value)} value={teamOf}/>
+                            <button onClick={e => setTeamOf(2)}><HiUsers/></button>
+                            <button onClick={e => setTeamOf(3)}><HiUserGroup/></button>
                         </div>
             }
             <div>
-                <p>movement list</p>
                 <label htmlFor="level">난이도</label>
                 <select id="level" name="level" onChange={e => setLevel(e.target.value)} value={level}>
                     <option value="rxd" defaultValue>Rx'd</option>
@@ -57,18 +117,41 @@ const WodCard = ({ data }) => {
                 { Array.from(movements).map((el, index) =>  <StyledMovementBox key={index}> 
                                                                 <h4>{el[1].name}</h4>
                                                                 <div>
-                                                                    <label htmlFor={`${el[1].id}-unit`}>Goal</label>
-                                                                    <input type="number" name={`${el[1].id}-unit`} id={`${el[1].id}-unit`}/>
-                                                                    <select name="unit" id="unit">
+                                                                    <label htmlFor={`${el[1].id}-goal`}>Goal</label>
+                                                                    <input 
+                                                                        type="number" 
+                                                                        name={el[1].id} 
+                                                                        id={`${el[1].id}-goal`}
+                                                                        min={1}
+                                                                        onChange={onChangeMovementData} 
+                                                                        value={movementData ? movementData[el[1].id]["goal"] : 1}
+                                                                    />
+                                                                    <select 
+                                                                        name={el[1].id} 
+                                                                        id={`${el[1].id}-goal-unit`} 
+                                                                        onChange={onChangeMovementData} 
+                                                                        value={movementData ? movementData[el[1].id]["goal-unit"] : ""}
+                                                                    >
                                                                         {el[1].unit.map((el, index) => <option key={index} value={el}>{el}</option>)}
                                                                     </select>
                                                                 </div>
                                                                 {
                                                                     el[1].weight &&
                                                                         <div>
-                                                                            <label htmlFor="weight">Weight</label>
-                                                                            <input type="number" name="weight" id="weight"/>
-                                                                            <select name="weight-unit" id="weight-unit">
+                                                                            <label htmlFor={`${el[1].id}-weight`}>Weight</label>
+                                                                            <input 
+                                                                                type="number" 
+                                                                                name={el[1].id} 
+                                                                                id={`${el[1].id}-weight`}
+                                                                                onChange={onChangeMovementData}
+                                                                                value={movementData ? movementData[el[1].id]["weight"] : 1}
+                                                                            />
+                                                                            <select 
+                                                                                name={el[1].id}
+                                                                                id={`${el[1].id}-weight-unit`}
+                                                                                onChange={onChangeMovementData}
+                                                                                value={movementData ? movementData[el[1].id]["weight-unit"] : ""}
+                                                                            >
                                                                                 <option value="lb" name="lb">lb</option>
                                                                                 <option value="kg" name="kg">kg</option>
                                                                             </select>
@@ -77,18 +160,15 @@ const WodCard = ({ data }) => {
                                                             </StyledMovementBox>
                 )}
                 <div>
-                    <label>Record WOD</label>
+                    <label htmlFor={type === "For Time of" ? "minutes" : "round"}>Record WOD</label>
                     {
-                    type === "For Time of" 
-                        ? <><input type="number" name="" id="" />:<input type="number" name="" id="" /></>
-                        : <><input type="number" name="" id="" /><span>Round</span></>
+                        type === "For Time of" 
+                            ? <><input type="number" name="minutes" id="minutes" />:<input type="number" name="second" id="second" /></>
+                            : <><input type="number" name="round" id="round" /><span>Round</span></>
                     }
                 </div>
-
-
-            
             </div>
-            <Button onClick={test} value="등록"/>
+            <Button onClick={createMovementData} value="등록"/>
         </StyledWodCard>
     );
 }
